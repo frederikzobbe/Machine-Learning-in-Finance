56af5f8848a7b55a0a968a8efd1992cca3936dae
frederikzobbe <frede_christiansen@hotmail.com>
2022-05-11 15:08:39 +0200

    Create Financial project.py
    The project is created

---
+++ Financial project.py
@@ -0,0 +1,14 @@
+# Title: Financial project
+# Github folder: Machine Learning in Finance
+# Created: 11-05-2022
+# Last time changed: 29-04-2022 
+
+################### Changelog ###############################
+# 11-05-2022: FZC: The project is defined
+# dd-mm-yyyy: xxx: 
+#############################################################
+
+#############################################################
+# This program is the workspace for the final project in    #
+# Applied Machine Learning.                                 #
+#############################################################
