d9707627cff9abc7d39e561bb2515050588fe7f5
frederikzobbe <frede_christiansen@hotmail.com>
2022-05-25 10:35:00 +0200

    Create Reading in the data.py
    Initializing

---
+++ Reading in the data.py
@@ -0,0 +1,14 @@
+# Navn: Reading in data
+# Oprettet: 25-05-2022
+# Senest ændret: 25-05-2022
+
+################### CHANGELOG ###########################
+# FZC: Created the document                             #
+#########################################################
+
+################### DESCRIPTION #########################
+# This program is reading in the data for the           #
+# final project in Applied Machine Learning             #
+#########################################################
+
+# 1. Reading in packages
