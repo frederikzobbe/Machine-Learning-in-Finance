f3721f7ecc8dbce1a6b67f6464af4255c7644f43
frederikzobbe <frede_christiansen@hotmail.com>
2022-06-12 22:15:13 +0200

    Update Modifying swissdata.py
    Small changes

--- Modifying swissdata.py
+++ Modifying swissdata.py
@@ -64,107 +64,20 @@ os.chdir("/Users/frederikzobbe/Desktop/Data")
 #os.chdir("/Users/frederikzobbe/Documents/Universitet/Forsikringsmatematik/Applied Machine Learning/Final project/Final project data/SwissData")
 
 #os.chdir("/Users/frederikzobbe/Documents/Universitet/Forsikringsmatematik/Applied Machine Learning/Final project/Final project data/SwissData")
-df1 = pd.read_csv('DEU.IDXEUR_Candlestick_1_Hour_BID_01.01.2018-30.04.2022.csv')
-DAX = df1
-DAX['Name'] = "DAX"
-DAX['Type'] = "Index"
-df2 = pd.read_csv('USA500.IDXUSD_Candlestick_1_Hour_BID_01.01.2018-30.04.2022.csv')
-SP = df2
-SP['Name'] = "S&P"
-SP['Type'] = "Index"
-df3 = pd.read_csv('USATECH.IDXUSD_Candlestick_1_Hour_BID_01.01.2018-30.04.2022.csv')
-NAS = df3
-NAS['Name'] = "NASDAQ"
-NAS['Type'] = "Index"
-df4 = pd.read_csv('HKG.IDXHKD_Candlestick_1_Hour_BID_01.01.2018-30.04.2022.csv')
-HK = df4
-HK['Name'] = "HK"
-HK['Type'] = "Index"
-df5 = pd.read_csv('GBR.IDXGBP_Candlestick_1_Hour_BID_01.01.2018-30.04.2022.csv')
-FTSE = df5
-FTSE['Name'] = "FTSE"
-FTSE['Type'] = "Index"
+df1 = pd.read_csv('BTCUSD_Candlestick_1_Hour_BID_08.05.2017-30.04.2022.csv')
+BIT = df1
+BIT['Name'] = "BITCOIN"
+BIT['Type'] = "Valuta"
+df2 = pd.read_csv('ETHUSD_Candlestick_1_Hour_BID_12.12.2017-30.04.2022.csv')
+ETH = df2
+ETH['Name'] = "ETHER"
+ETH['Type'] = "Valuta"
+df3 = pd.read_csv('EURUSD_Candlestick_1_Hour_BID_01.01.2013-30.04.2022.csv')
+EUR = df3
+EUR['Name'] = "EUROUSD"
+EUR['Type'] = "Valuta"
+data = pd.concat([BIT, ETH, EUR])
 
-#os.chdir("/Users/frederikzobbe/Documents/Universitet/Forsikringsmatematik/Applied Machine Learning/Final project/Final project data/SwissData")
-#date_cols_tmp = ['Local time']
-#df1 = pd.read_csv('SwissData.txt', index_col=None, parse_dates=date_cols_tmp)
+data = timefunc(data, 0)
+data.to_csv('ValutaDataHour.txt', index = False, header=True)
 
-# 4. Applying functions to data
-df = pd.concat([DAX, HK, SP, NAS, FTSE], ignore_index=True)
-data = timefunc(df, 0)
-data['Name'].value_counts()
-data.to_csv('IndexDataHour.txt', index = False, header=True)
-
-# 4.5
-os.chdir("/Users/frederikzobbe/Desktop/Data")
-data = timefunc(df, 0)
-
-# 5. Test of results
-ticker = 'DAX'
-df_sub = swissdata[(swissdata['Name'] == ticker) & (swissdata['Year'] < 2019) & (swissdata['Month'] < 3)]
-df_sub.head(5)
-len(df_sub)
-
-x = df_sub['Local time']
-y = df_sub['Close']
-z = df_sub['CET']
-
-plt.subplot(1,2,1)
-plt.plot(x,y,'r')
-plt.subplot(1,2,2)
-plt.plot(z,y,'b')
-plt.show()
-
-import plotly.express as px
-fig = px.line(df_sub, x="CET", y="Close", color='Name')
-fig.show()
-
-## ---------------------------- Michael Modifying 20 min index data ----------------------------
-os.chdir("/Users/mikki/Desktop/AppML/New env Personal projects/Final project/20minIndex")
-
-df1 = pd.read_csv('DEU.IDXEUR_Candlestick_20_M_BID_01.01.2018-31.12.2019.csv')
-df2 = pd.read_csv('DEU.IDXEUR_Candlestick_20_M_BID_01.01.2020-30.04.2022.csv')
-
-DAX = pd.concat([df1, df2]).reset_index(drop = True)
-DAX['Name'] = "DAX"
-DAX['Type'] = "Index"
-
-df1 = pd.read_csv('HKG.IDXHKD_Candlestick_20_M_BID_01.01.2018-31.12.2019.csv')
-df2 = pd.read_csv('HKG.IDXHKD_Candlestick_20_M_BID_01.01.2020-30.04.2022.csv')
-HK = pd.concat([df1, df2]).reset_index(drop = True)
-HK['Name'] = "HK"
-HK['Type'] = "Index"
-
-df1 = pd.read_csv('USA500.IDXUSD_Candlestick_20_M_BID_01.01.2018-31.12.2020.csv')
-df2 = pd.read_csv('USA500.IDXUSD_Candlestick_20_M_BID_01.01.2021-30.04.2022.csv')
-SP = pd.concat([df1, df2]).reset_index(drop = True)
-SP['Name'] = "S&P"
-SP['Type'] = "Index"
-
-df1 = pd.read_csv('USATECH.IDXUSD_Candlestick_20_M_BID_01.01.2018-31.12.2019.csv')
-df2 = pd.read_csv('USATECH.IDXUSD_Candlestick_20_M_BID_01.01.2020-30.04.2022.csv')
-NAS = pd.concat([df1, df2]).reset_index(drop = True)
-NAS['Name'] = "NASDAQ"
-NAS['Type'] = "Index"
-
-df1 = pd.read_csv('GBR.IDXGBP_Candlestick_20_M_BID_01.01.2018-31.12.2019.csv')
-df2 = pd.read_csv('GBR.IDXGBP_Candlestick_20_M_BID_01.01.2020-30.04.2022.csv')
-FTSE = pd.concat([df1, df2]).reset_index(drop = True)
-FTSE['Name'] = "FTSE"
-FTSE['Type'] = "Index"
-
-
-# I Need to add 1 hour to my data in order to ensure time consistency with our other datasets.
-FTSE.dtypes
-IndexdataHour_DB.dtypes
-
-IndexDat20Min = pd.concat([DAX, FTSE, SP, NAS, HK]).reset_index(drop = True)
-IndexDat20Min.dtypes
-IndexDat20Min['Gmt time'] = pd.to_datetime(IndexDat20Min['Gmt time'].str[:-4], format= "%d.%m.%Y %H:%M:%S")
-IndexDat20Min['CET'] = IndexDat20Min['Gmt time'] + pd.DateOffset(hours = 1)
-IndexDat20Min["Year"] = IndexDat20Min.CET.dt.year
-IndexDat20Min['Month'] = IndexDat20Min.CET.dt.month
-IndexDat20Min['Day'] = IndexDat20Min.CET.dt.day
-
-os.chdir("/Users/mikki/Desktop/AppML/New env Personal projects/Final project/data/SwissData")
-IndexDat20Min.to_csv("20MinIndexData", index = None, index_label=None)
